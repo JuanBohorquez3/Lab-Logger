8ebef95b443c9ddcdf214e727344051b2d09483c
JuanBohorquez3 <jbohorquez@wisc.edu>
2021-02-03 13:39:28 -0600

    Last commit

--- recorder.py
+++ recorder.py
@@ -6,11 +6,45 @@ import os
 import sys
 import time
 
+# TODO : Figure out a half decent way of doing this import
+origin_path = r"C:\Users\Hybrid\Repos\Origin"
+sys.path.append(os.path.join(origin_path, "lib"))
 # Local imports
-#import origin
+from origin.client import server
 from logger_setup import setup_root_logger
+from stream import Stream, Field
+from instruments import Instrument
 
 DATA = 15
+
+def make_streams():
+    MOT_I2V_fields = {}
+    units = "mW"
+    conversion_desc = "V to mW"
+    MOT_I2V_fields["Y1"] = Field(
+        "Y1",
+        "ai0",
+        lambda v: 0.902*v+0.022,
+        units,
+        conversion_desc
+    )
+    MOT_I2V_fields["Y2"] = Field(
+        "Y2",
+        "ai1",
+        lambda v: 0.849*v+0.028,
+        units,
+        conversion_desc
+    )
+    MOT_I2V_fields["X1"] = Field(
+        "X1",
+        "ai4",
+        lambda v: 0.740*v+0.026,
+        units,
+        conversion_desc
+    )
+
+    MOT_stream = Stream("MOT I2Vs, ")
+
 
 def recorder():
     """
@@ -32,4 +66,5 @@ if __name__ == "__main__":
     root_log.info("INFO")
     root_log.debug("DEBUG")
     root_log.data("DATA")
+
     recorder()
--- stream.py
+++ stream.py
@@ -3,40 +3,64 @@ Holds steam class to organize connection to both origin server and data device
 for a given data stream.
 """
 # external imports
-from typing import Dict, Tuple, Callable, Union
+from typing import Dict, Tuple, Callable, Union, TypeVar
 import logging
-import colorlog
 from inspect import signature
 
 # local imports
 from logger_setup import setup_child_logger
-from instruments import Instrument
+
+T = TypeVar('T')
 
 
 class Field:
-    """
-    Class to contain a data field that exists within a data stream and all it's attributes
+    """Class to contain a data field that exists within a data stream and all it's attributes
 
-    Attributes
+        Typical usage example:
+
+        field = Field(
+            "X2",
+            "ai2",
+            lamda x : 0.55*x+0.022,
+            unit = "mW",
+            conversion_desc = "linear calibration from V to mW"
+        )
+
+    Attributes:
+        name - name of the field
+        address - address corresponding to the field on the field's measurement device
+        conversion - callable of a single argument that returns the converts the measured data from
+            an instrument to data in this field's desired units. Must be a callable of a single
+            argument
+        unit - optional attribute specifying the units of the data to be posted to the server.
+            For example, X2 would have unit = "mW" or "milliwatts". Only used when logging or
+            plotting data.
+        conversion_desc - description of the conversion function.
     """
-    def __init__(self, name: str, address: str, conversion: Callable = None, unit: str = ""):
+    def __init__(
+            self,
+            name: str,
+            address: str,
+            conversion: Callable[[T], T] = None,
+            unit: str = "",
+            conversion_desc: str = ""):
         self.__name = str(name)
         self.__address = str(address)
         self.__unit = str(unit)
 
-        self.logger = setup_child_logger(self.name)
+        self._logger = setup_child_logger(str(self))
 
         # Buncha warnings and typing errors
         if not isinstance(name, str):
-            self.logger.warning(f"Name provided was not a string, name cast as {self.name}")
+            self._logger.warning(f"Name provided was not a string, name cast as {self.name}")
         if not isinstance(address, str):
-            self.logger.warning(
+            self._logger.warning(
                 f"Address provided was not a string, address cast as {self.address}")
         if not isinstance(unit, str):
-            self.logger.warning(f"units provided was not a string. units cast as {self.unit}")
+            self._logger.warning(f"units provided was not a string. units cast as {self.unit}")
 
         # make sure our conversion function is a function of a single variable.
-        conversion = conversion if conversion is not None else (lambda x: x)
+        conversion = conversion if conversion else (lambda x: x)
         if not callable(conversion):
             raise TypeError(
                 f"{conversion} is not callable. Provide a function of a single argument"
@@ -48,6 +72,7 @@ class Field:
             )
 
         self.__conversion = conversion
+        self.__conversion_desc = conversion.__doc__ if conversion.__doc__ else conversion_desc
 
     @property
     def name(self) -> str:
@@ -75,14 +100,14 @@ class Field:
         return self.__unit
 
     @property
-    def conversion_doc(self) -> Union[str,None]:
+    def conversion_desc(self) -> Union[str,None]:
         """
         Docstring attached to conversion function, if any is provided.
 
         Suggested docstring indicates conversion formula being used, device measured units, units of
             converted data
         """
-        return self.__conversion.__doc__
+        return self.__conversion_desc
 
     def __str__(self):
         return f"{self.__class__.__name__} : {self.name}"
@@ -90,13 +115,14 @@ class Field:
 
 class Stream:
     """
+    Represents a data stream to origin.
+
     Attributes:
         stream_name : Name of data stream in origin server this channel will manage. Must be
             formatted as {NAMESPACE}_{descriptive string}
         fields : dictionary of field names to Field objects
         data_type : type of data being sent to the server. eg. float, or bool
         server : origin.server object. Interfaces with the origin server
-        instrument : Instrument object communicating with our physical measurement device
     """
     TIMEOUT = 60*1000  # server timeout time in ms
     NAMESPACE = "Hybrid"  # TODO : configure this with config file or GUI setting
@@ -106,25 +132,23 @@ class Stream:
             stream_name: str,
             fields: Dict[str: Field],
             data_type: type,
-            server: origin.server,
-            instrument: Instrument):
+            server: origin.server
+    ):
         # use root logger temporarily
         self.logger = logging.getLogger(self.__class__.__name__)
         """
-        TODO : imports for origin and Monitors
+        TODO : imports for origin
         """
         # TODO : ensure that name is formatted correctly:
         #   {Channel.NAMESPACE}_{descriptive name}
         self.__stream_name = stream_name
-        setup_child_logger(str(self))
+        self.logger = setup_child_logger(str(self))
 
         self.__fields = fields
 
         if data_type not in [int, float, bool, str]:
             raise ValueError("data_type must be int, float, bool or str")
         self.__data_type = data_type
-
-        self.__instrument = instrument
 
         self.server = server
 
@@ -164,12 +188,6 @@ class Stream:
     def fields(self) -> Dict[str: Field]:
         return self.__fields
 
-    @property
-    def instrument(self) -> Instrument:
-        return self.__instrument
-
-    @property
-
     def connect(self):
         """
         Initiates connection with the server. Passes info on stream name and data types of the
@@ -186,14 +204,11 @@ class Stream:
         )
         return connection
 
-    def measure(self) -> Dict[str: float]:
+    def write(self, data: Dict[Field, ]):
         """
-        Instructs our device to measure the data and returns said data
-        Returns:
-            measured data mapping field names to their measured values
+        Writes data to the origin server
         """
-        self.data = self.__instrument.measure(self.stream_name)
-        return self.data
+        pass  # TODO
 
     def close(self) -> Tuple[int,int]:
         """
@@ -201,9 +216,7 @@ class Stream:
         Returns:
             error codes provided by either the server and device, in that order
         """
-        err_serv = self.connection.close()
-        err_dev = self.__instrument.close()
-        return err_serv, err_dev
+        return self.server.close
 
     def __str__(self):
         return f"{self.__class__.__name__} : {self.stream_name}"
